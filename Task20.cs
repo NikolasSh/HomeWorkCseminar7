8d355a728195f46e114355bb3012ca394506ebb3
Nikolai <107433192+NikolasSh@users.noreply.github.com>
2023-02-19 12:24:32 +0300

    Add files home work

---
+++ Task20.cs
@@ -0,0 +1,85 @@
+﻿/*
+Напишите программу, которая на вход принимает позиции элемента в 
+двумерном массиве, и возвращает значение этого элемента 
+или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result ) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено неверное число! Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int[,] InitMatrix()
+{
+    int[,] matrix = new int[3,4];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i,j] = rnd.Next(1,50);
+                }
+        }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+            Console.WriteLine();
+        }       
+}
+
+void FindNumber(int[,] matrix)
+{
+    int result = GetNumber("Введите число: ");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i,j] == result)
+            {
+                Console.WriteLine($"Число {result} есть в массиве!");
+                return;                
+            }
+            
+            
+        }
+    }
+
+    Console.WriteLine($"Числа {result} нет в массиве!");
+}
+
+
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+FindNumber(matrix);
---
+++ Task21.cs
@@ -0,0 +1,58 @@
+﻿/*
+Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+double[,] InitMatrix()
+{
+    double[,] matrix = new double[3,4];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i,j] = rnd.Next(1,20);
+                }
+        }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+            Console.WriteLine();
+        }       
+}
+
+void Average(double[,] matrix)
+{
+    double[] array = new double[4];
+
+    array[0] = (matrix[0,0] + matrix[1,0] + matrix[2,0]) /3;
+    array[1] = (matrix[0,1] + matrix[1,1] + matrix[2,1]) /3;
+    array[2] = (matrix[0,2] + matrix[1,2] + matrix[2,2]) /3;
+    array[3] = (matrix[0,3] + matrix[1,3] + matrix[2,3]) /3;
+    
+    Console.WriteLine($"Среднее арифметическое первого столбца массива = {Math.Round(array[0], 1)}");
+    Console.WriteLine($"Среднее арифметическое второго столбца массива = {Math.Round(array[1], 1)}");
+    Console.WriteLine($"Среднее арифметическое третьего столбца массива = {Math.Round(array[2], 1)}");
+    Console.WriteLine($"Среднее арифметическое четвертого столбца массива = {Math.Round(array[3], 1)}"); 
+}
+
+
+double[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+Average(matrix);
